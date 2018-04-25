216067de0439fd0dabb753bf2546940e36c41300
msserpa <matheusserpa@gmail.com>
2018-04-25 14:14:13 -0300

    workable version

--- run/EXEC.sh
+++ run/EXEC.sh
@@ -6,7 +6,7 @@ then
 	echo "Usage : $0 <size>"
 	exit 1
 fi
-
+export ACC_DEVICE_NUM=1;
 rsync -a -v ../src/ModelagemFletcher.exe .
 time ./ModelagemFletcher.exe "ISO" $1 $1 $1 32 12.5 12.5 12.5 0.0010 0.2
 
