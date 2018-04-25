6ff3e5df5fd5b5b4a1a004601a71ab807d11c54e
msserpa <matheusserpa@gmail.com>
2018-04-25 08:13:54 -0300

    change execution parameters

--- run/EXEC.sh
+++ run/EXEC.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # <execname> <fNameSec> <nx> <ny> <nz> <absorb> <dx> <dy> <dz> <dt> <tmax>
-time ./ModelagemFletcher.exe "VTI" 241 241 241 16 12.5 12.5 12.5 0.0010 0.2
+
+if [ $# -lt 1 ]
+then
+	echo "Usage : $0 <size>"
+	exit 1
+fi
+
+rsync -a -v ../src/ModelagemFletcher.exe .
+time ./ModelagemFletcher.exe "ISO" $1 $1 $1 32 12.5 12.5 12.5 0.0010 0.2
 
